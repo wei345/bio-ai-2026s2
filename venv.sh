f03d9f765e67c90a4f76b38104c66fd67bd5c616
liuwei <weiliu345@gmail.com>
2026-09-25 02:31:40 +1200

    refactor: organize test files, fixtures, and project configuration

--- venv.sh
+++ venv.sh
@@ -1,11 +1,9 @@
 
-# python 3.11
+# python 3.11 was used for this project
+
 python3 -m venv .venv
 source .venv/bin/activate
 
 pip install opencv-python==5.0.0.93 mediapipe==0.10.14 numpy==2.4.6 scipy==1.17.1
 
-pip install ipykernel
-
-# For web UI
 pip install Flask==3.1.3 werkzeug==3.1.8
